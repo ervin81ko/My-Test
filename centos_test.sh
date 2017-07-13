f2228aad3fd2a39dc793cbf96a318f4ce16a437f
ervin81ko <ervin.konomi@pd.infn.it>
2017-07-13 12:28:02 +0200

    Update centos_test.sh

--- centos_test.sh
+++ centos_test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 sudo mkdir /root/synergy_scripts 
 sudo cat<< EOF >/etc/cron.d/synergy_cron.txt
 */1 * * * * root /root/synergy_scripts/check_expiration_time.sh
 EOF
 sudo chmod 644 /root/synergy_cron.txt
-
 #Check expiartion time
 sudo cat <<'EOF'>> /root/synergy_scripts/check_expiration_time.sh
 #!/bin/bash
@@ -13,6 +11,5 @@ expiration_date=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json
 syn_allert_clock=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=syn_allert_clock=).*')
 echo $syn_allert_clock > /root/synergy_scripts/pippo.txt
 echo $expiration_date > /root/synergy_scripts/pluto.txt
-
 EOF
 sudo chmod 755 /root/synergy_scripts/check_expiration_time.sh#!/bin/bash
