7dc7666ac9bffcfdf5f1cd08c291487944c2a961
ervin81ko <ervin.konomi@pd.infn.it>
2017-07-13 16:23:06 +0200

    Update centos_test.sh

--- centos_test.sh
+++ centos_test.sh
@@ -1,17 +1,50 @@
+
 #!/bin/bash
-sudo mkdir /root/synergy_scripts 
-# Synergy cron
+###########
+# Centos7 #
+###########
+
+# Synergy directory
+sudo mkdir /root/synergy_scripts
+
+# Cron 
 sudo cat<< EOF >/etc/cron.d/synergy_cron.txt
 */1 * * * * root /root/synergy_scripts/check_expiration_time.sh
 EOF
-sudo chmod 644 /etc/cron.d/synergy_cron.txt
+sudo chmod 644 /root/synergy_cron.txt
+
+#Make user script
+sudo cat <<'EOF'>> /root/synergy_scripts/user_script.sh
+#!/bin/bash
+EOF
+row_number=$(curl -s http://169.254.169.254/openstack/latest/user_data |grep -m1 -n '#!\/bin\/bash' | sed 's/^\([0-9]\+\):.*$/\1/')
+curl -s http://169.254.169.254/openstack/latest/user_data | tail -n +$row_number > /root/synergy_scripts/user_script.sh
+sudo chmod 755 /root/synergy_scripts/user_script.sh
 
 #Check expiartion time
 sudo cat <<'EOF'>> /root/synergy_scripts/check_expiration_time.sh
 #!/bin/bash
-expiration_date=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_date\": \")[^\"]+")
-syn_allert_clock=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=syn_allert_clock=).*')
-echo $syn_allert_clock > /root/synergy_scripts/pippo.txt
-echo $expiration_date > /root/synergy_scripts/pluto.txt
-EOF
-sudo chmod 755 /root/synergy_scripts/check_expiration_time.sh
+
+#Expiration time in sec. since 1970-01-01 00:00:00 UTC
+#expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_date\": \")[^\"]+")
+expiration_time=$(date +%s -d'2017-07-12 12:35:00')
+
+#Time in min.
+#time_allert=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=syn_allert_clock=).*')
+
+time_allert=2
+
+#Current time in sec. since 1970-01-01 00:00:00 UTC
+curr_time=$(date -u +%s)
+
+#Compute the difference time in min.
+let "time_diff=($expiration_time-$curr_time)/60"
+
+if [ "$time_diff" -le "$time_allert" ]
+then
+      sudo ./user_script.sh
+
+else
+      echo "expression evaluated as false nothing to do"
+
+fi
