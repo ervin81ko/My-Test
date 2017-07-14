b3bae61c00d3f95f4f2352a0503927b76b333d58
ervin81ko <ervin.konomi@pd.infn.it>
2017-07-14 10:53:33 +0200

    Update centos_test.sh

--- centos_test.sh
+++ centos_test.sh
@@ -18,7 +18,7 @@ sudo cat <<'EOF'>> /root/synergy_scripts/user_script.sh
 #!/bin/bash
 EOF
 sudo chmod 777 /root/synergy_scripts/user_script.sh
-row_number=$(curl -s http://169.254.169.254/openstack/latest/user_data |grep -m2 -n '#!\/bin\/bash' | sed 's/^\([0-9]\+\):.*$/\1/')
+row_number=$(curl -s http://169.254.169.254/openstack/latest/user_data |grep -m1 -n '#!\/bin\/bash' | sed 's/^\([0-9]\+\):.*$/\1/')
 curl -s http://169.254.169.254/openstack/latest/user_data | tail -n +$row_number > /root/synergy_scripts/user_script.sh
 
 #Check expiartion time
