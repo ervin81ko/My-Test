1804c7fd6accf2c926625c27959a7278c50a16ea
ervin81ko <ervin.konomi@pd.infn.it>
2017-07-19 16:21:31 +0200

    Update centos_test.sh

--- centos_test.sh
+++ centos_test.sh
@@ -10,12 +10,11 @@ cat << EOF >/etc/cron.d/synergy_cron.txt
 EOF
 chmod 644 /etc/cron.d/synergy_cron.txt
 
+dest_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=dest_path=).*')
+
 # Check expiartion time
 cat << 'EOF' >> /root/synergy_scripts/check_expiration_time.sh
 #!/bin/bash
-
-dest_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=dest_path=).*')
-
 # Expiration time in sec. since 1970-01-01 00:00:00 UTC
 # expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_date\": \")[^\"]+")
 expiration_time=$(date +%s -d'2017-07-12 12:35:00')
@@ -33,8 +32,6 @@ let "time_diff=($expiration_time-$curr_time)/60"
 
 if [ "$time_diff" -le "$time_allert" ]
 then
-EOF
-cat << EOF >> /root/synergy_scripts/check_expiration_time.sh
     $dest_path
 else
     echo "expression evaluated as false nothing to do"
