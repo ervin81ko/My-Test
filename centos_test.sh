09342a40bf756ab1b55bab526e09cfe5bf6b93ee
ervin81ko <ervin.konomi@pd.infn.it>
2017-07-19 15:19:54 +0200

    Update centos_test.sh

--- centos_test.sh
+++ centos_test.sh
@@ -4,20 +4,17 @@
 # Centos7 #
 ###########
 
-# Synergy directory
-mkdir /root/synergy_scripts
-
 # Cron 
 cat << EOF >/etc/cron.d/synergy_cron.txt
 */2 * * * * root /root/synergy_scripts/check_expiration_time.sh
 EOF
 chmod 644 /etc/cron.d/synergy_cron.txt
 
-dest_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=dest_path=).*')
-
 # Check expiartion time
 cat << 'EOF' >> /root/synergy_scripts/check_expiration_time.sh
 #!/bin/bash
+
+dest_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=dest_path=).*')
 
 # Expiration time in sec. since 1970-01-01 00:00:00 UTC
 # expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_date\": \")[^\"]+")
