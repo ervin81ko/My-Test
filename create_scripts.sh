9e8d463f891cbeb19aa1163a0af85cb7a5005808
ervin81ko <ervin.konomi@pd.infn.it>
2017-09-14 17:07:41 +0200

    Update create_scripts.sh

--- create_scripts.sh
+++ create_scripts.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Make Synergy cron 
+
 cat << EOF >/etc/cron.d/synergy_cron
 */1 * * * * root /root/synergy_scripts/check_expiration_time.sh
 EOF
+
+if [ $? -eq 0 ]; then 
+  echo `date`" 'synergy_cron' file created correctly" >>/root/synergy_scripts/log.txt;
+else
+  echo `date`" Error: 'synergy_cron file not created" >>/root/synergy_scripts/log.txt;
+fi
 
 dest_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=dest_path=).*')
 
@@ -13,8 +20,14 @@ cat << 'EOF' >> /root/synergy_scripts/check_expiration_time.sh
 # Expiration time in sec. since 1970-01-01 00:00:00 UTC
 expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_time\": \")[^\"]+")
 
+if [ $? -ne 0 ]; then 
+  echo `date`" Error: 'expiration_time' variable not valorized" >>/root/synergy_scripts/log.txt;
+fi
 # Time in min.
 time_allert=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=syn_allert_clock=).*')
+if [ $? -ne 0 ]; then 
+  echo `date`" Error: 'time_allert' variable not valorized" >>/root/synergy_scripts/log.txt;
+fi
 
 # Current time in sec. since 1970-01-01 00:00:00 UTC
 curr_time=$(date -u +%s)
@@ -33,7 +46,12 @@ cat <<'EOF'>> /root/synergy_scripts/check_expiration_time.sh
       rm -rf /etc/cron.d/synergy_cron; 
     fi
 else
-    echo "Expiration time checked on:" `date` >>/root/synergy_scripts/expiration_time_log.txt
+    echo "Expiration time checked on:" `date` >>/root/synergy_scripts/log.txt
 fi
 EOF
+if [ $? -eq 0 ]; then 
+  echo `date`" 'check_expiration_time' script created correctly " >>/root/synergy_scripts/log.txt;
+else
+  echo `date`" Error: 'check_expiration_time' script not created" >>/root/synergy_scripts/log.txt;
+fi
 chmod 755 /root/synergy_scripts/check_expiration_time.sh
