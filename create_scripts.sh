6c07fd806b2049407fa3f6b83a0ad4b30c68ffe7
ervin81ko <ervin.konomi@pd.infn.it>
2017-09-15 16:08:53 +0200

    Update create_scripts.sh

--- create_scripts.sh
+++ create_scripts.sh
@@ -2,7 +2,7 @@
 echo `date`" info: Starting.." >>/root/synergy_scripts/log.txt;
 # Create Synergy cron file
 cat << EOF >/etc/cron.d/synergy_cron
-*/1 * * * * root /root/synergy_scripts/check_expiration_time.sh
+*/1 * * * * root /root/synergy_scripts/check_execution_time.sh
 EOF
 if [ $? -eq 0 ]; then 
   echo `date`" info: 'synergy_cron' file created correctly" >>/root/synergy_scripts/log.txt;
@@ -13,24 +13,27 @@ user_script_path=$(curl -s http://169.254.169.254/openstack/latest/user_data | g
 if [ $? -ne 0 ]; then 
   echo `date`" error: 'user_script_path' variable not valorized" >>/root/synergy_scripts/log.txt;
 fi
+
 # Check user script creation
 if [[ -x "$user_script_path" ]]; then
   echo `date`" info: user script created correctly" >>/root/synergy_scripts/log.txt;
 fi
+
 # Create check execution time script
 cat << 'EOF' >> /root/synergy_scripts/check_execution_time.sh
 #!/bin/bash
 # Expiration time in sec. since 1970-01-01 00:00:00 UTC
 expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_time\": \")[^\"]+")
-
 if [ $? -ne 0 ]; then 
   echo `date`" error: 'expiration_time' variable not valorized" >>/root/synergy_scripts/log.txt;
 fi
+
 # Time in min.
 syn_clock=$(curl -s http://169.254.169.254/openstack/latest/user_data | grep -m1 -oP '(?<=syn_clock=).*')
 if [ $? -ne 0 ]; then 
   echo `date`" error: 'syn_clock' variable not valorized" >>/root/synergy_scripts/log.txt;
 fi
+
 # Current time in sec. since 1970-01-01 00:00:00 UTC
 curr_time=$(date -u +%s)
 
