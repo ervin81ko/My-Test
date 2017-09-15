340d55159ad9e9e347124e3a7920a1612c6866c0
ervin81ko <ervin.konomi@pd.infn.it>
2017-09-15 15:38:25 +0200

    Update create_scripts.sh

--- create_scripts.sh
+++ create_scripts.sh
@@ -17,8 +17,8 @@ fi
 if [[ -x "$user_script_path" ]]; then
   echo `date`" info: user script created correctly" >>/root/synergy_scripts/log.txt;
 fi
-# Create check expiartion time script
-cat << 'EOF' >> /root/synergy_scripts/check_expiration_time.sh
+# Create check execution time script
+cat << 'EOF' >> /root/synergy_scripts/check_execution_time.sh
 #!/bin/bash
 # Expiration time in sec. since 1970-01-01 00:00:00 UTC
 expiration_time=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json | grep -oP "(?<=\"expiration_time\": \")[^\"]+")
@@ -36,26 +36,25 @@ curr_time=$(date -u +%s)
 
 # Compute the difference time in min.
 let "time_diff=($expiration_time-$curr_time)/60"
-
 if [ "$time_diff" -le "$syn_clock" ]
 then
 EOF
-cat <<EOF>> /root/synergy_scripts/check_expiration_time.sh
+cat <<EOF>> /root/synergy_scripts/check_execution_time.sh
     $user_script_path
 EOF
-cat <<'EOF'>> /root/synergy_scripts/check_expiration_time.sh
+cat <<'EOF'>> /root/synergy_scripts/check_execution_time.sh
     if [ $? -eq 0 ]; then 
       echo `date`" info: user script executed correctly" >>/root/synergy_scripts/log.txt;
       rm -rf /etc/cron.d/synergy_cron; 
       echo `date`" info: 'synergy_cron' file removed correctly" >>/root/synergy_scripts/log.txt;
     fi
 else
-    echo `date`" info: expiration time checked" >>/root/synergy_scripts/log.txt;
+    echo `date`" info: execution time checked" >>/root/synergy_scripts/log.txt;
 fi
 EOF
 if [ $? -eq 0 ]; then 
-  echo `date`" info: 'check_expiration_time' script created correctly " >>/root/synergy_scripts/log.txt;
+  echo `date`" info: 'check_execution_time' script created correctly " >>/root/synergy_scripts/log.txt;
 else
-  echo `date`" error: 'check_expiration_time' script not created" >>/root/synergy_scripts/log.txt;
+  echo `date`" error: 'check_execution_time' script not created" >>/root/synergy_scripts/log.txt;
 fi
-chmod 755 /root/synergy_scripts/check_expiration_time.sh
+chmod 755 /root/synergy_scripts/check_execution_time.sh
